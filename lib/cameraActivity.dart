e06224958472fb86924d32a2ed10ea87408f7432
PRATHAMESH <pmsonar2000@gmail.com>
2020-12-06 23:43:08 +0530

    tried to implement capture image in take_picture_page.dart

--- lib/cameraActivity.dart
+++ lib/cameraActivity.dart
@@ -1,6 +1,9 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
 
 void main() => runApp(MaterialApp(
       theme:
@@ -17,6 +20,7 @@ class _CameraActivityState extends State<CameraActivity> {
   List<CameraDescription> cameras;
   CameraController controller;
   bool isReady = false;
+  String _path = null;
 
   @override
   void initState() {
@@ -39,6 +43,23 @@ class _CameraActivityState extends State<CameraActivity> {
     });
   }
 
+  void _takePicture(BuildContext context) async {
+    try {
+
+
+      final path =
+      join((await getTemporaryDirectory()).path, '${DateTime.now()}.png');
+
+      await controller.takePicture(path);
+
+      Navigator.pop(context,path);
+
+    } catch (e) {
+      print(e);
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,11 +74,14 @@ class _CameraActivityState extends State<CameraActivity> {
               aspectRatio: controller.value.aspectRatio,
               child: CameraPreview(controller),
             ),
+
             ButtonTheme(
               minWidth: 200.0,
               height: 150.0,
               child: RaisedButton(
-                onPressed: () {},
+                onPressed: () {
+                  _takePicture(context);
+                },
                 color: Colors.blueAccent,
                 child: Icon(
                   Icons.camera_alt,
@@ -69,5 +93,11 @@ class _CameraActivityState extends State<CameraActivity> {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
